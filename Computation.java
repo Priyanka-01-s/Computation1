fb1ed89626634844c8ef82253009c6369b974b85
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-21 23:04:31 +0530

    Length of a line

--- Computation.java
+++ Computation.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
+
 public class Computation {
+
+    public static double lineLength(double x1, double y1, double x2, double y2) {
+        return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    }
+
     public static void main(String[] args){
         System.out.println("Welcome to Line Comparison Computation Program");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the x-coordinate of the first point: ");
+        double x1 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the first point: ");
+        double y1 = scanner.nextDouble();
+        System.out.print("Enter the x-coordinate of the second point: ");
+        double x2 = scanner.nextDouble();
+        System.out.print("Enter the y-coordinate of the second point: ");
+        double y2 = scanner.nextDouble();
+
+        scanner.close();
+        double distance = lineLength(x1, y1, x2, y2);
+        System.out.println("Length of the lines: " + distance);
     }
 }
