cec2db79a39aefdb45c1bc273ce10e1974ae1d70
Priyanka Sengupta <78321696+Priyanka-01-s@users.noreply.github.com>
2023-11-23 15:36:18 +0530

    Compare lines

--- Computation.java
+++ Computation.java
@@ -1,49 +1,35 @@
 
-import java.util.Scanner;
-
 public class Computation {
-    //set endpoints
-    private static double[] setEndpoints(String lineName) {
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter x1 for " + lineName + ": ");
-        double x1 = scanner.nextDouble();
-        System.out.print("Enter y1 for " + lineName + ": ");
-        double y1 = scanner.nextDouble();
-        System.out.print("Enter x2 for " + lineName + ": ");
-        double x2 = scanner.nextDouble();
-        System.out.print("Enter y2 for " + lineName + ": ");
-        double y2 = scanner.nextDouble();
-        return new double[]{x1, y1, x2, y2};
-    }
-    private static boolean areEqual(double[] line1, double[] line2) {
-        return line1[0] == line2[0] && line1[1] == line2[1] &&
-               line1[2] == line2[2] && line1[3] == line2[3];
+    private double[] endpoints;
+
+    public Computation(double[] endpoints) {
+        this.endpoints = endpoints;
     }
 
-    private static int compareLines(double[] line1, double[] line2) {
-        if (areEqual(line1, line2)) {
-            return 0; 
-        } else if (calculateLength(line1) > calculateLength(line2)) {
-            return 1;  // Line 1 is greater
-        } else {
-            return -1;  // Line 2 is greater
-        }
+   
+    public static int compare(Computation line1, Computation line2) {
+        double length1 = calculateLength(line1.endpoints);
+        double length2 = calculateLength(line2.endpoints);
+        return Double.compare(length1, length2);
     }
-    //cal length
+
     private static double calculateLength(double[] line) {
         return Math.sqrt(Math.pow(line[2] - line[0], 2) + Math.pow(line[3] - line[1], 2));
     }
 
     public static void main(String[] args) {
-        
         System.out.println("Welcome to Line Comparison Computation Program");
-        double[] line1 = setEndpoints("Line 1");
-        double[] line2 = setEndpoints("Line 2");
 
-        if (areEqual(line1, line2)) {
+        double[] line1 = {1.0, 2.0, 4.0, 6.0};  
+        double[] line2 = {9.0, 1.0, 3.0, 4.0};  
+
+        Computation lineObj1 = new Computation(line1);
+        Computation lineObj2 = new Computation(line2);
+
+        if (lineObj1.equals(lineObj2)) {
             System.out.println("Lines are equal.");
         } else {
-            int result = compareLines(line1, line2);
+            int result = compare(lineObj1, lineObj2);
 
             if (result > 0) {
                 System.out.println("Line 1 is greater than Line 2.");
